ad45d5769851fb9063a9f0781404283fcb43ef36
ArsalanKhan04 <arsal4an@gmail.com>
2022-11-11 16:18:20 +0500

    Entry page and Index page are implemented without Markdown

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request, title):
+    if util.get_entry(title) == None:
+        return render(request, "encyclopedia/pagenotfound.html")
+    return render(request, "encyclopedia/page.html", {
+        "title" : title,
+        "content": util.get_entry(title)
+    })
+
